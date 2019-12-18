06025c8dfce178bc506aca96eeebd01b9d4a7f99
LeventDemir <leventdemir518@gmail.com>
2019-12-18 14:32:53 +0300

    login page created

---
+++ pages/login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="col s12">
+    <div class="card">
+      <div class="card-content">
+        <div class="row">
+          <div class="input-field col s12">
+            <i class="material-icons prefix">person</i>
+            <input id="username" type="text" class="validate" />
+            <label for="username">Username</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <i class="material-icons prefix">lock</i>
+            <input id="password" type="tel" class="validate" />
+            <label for="password">Password</label>
+          </div>
+        </div>
+        <div class="row center">
+          <button class="btn indigo">Login</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
